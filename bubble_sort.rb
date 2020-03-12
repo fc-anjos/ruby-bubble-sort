286d6b96bb6907bdf998aac43864522848935e24
fc-anjos <felipe.anjos@usp.br>
2020-03-12 20:52:06 -0300

    add bubble_sort_by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -14,7 +14,23 @@ def bubble_sort(array)
   array
 end
 
-array = Array.new(10) { rand(1...9) }
-puts " The original array is: #{array} "
-bubble_sort(array)
-puts " The sorted array is: #{array} "
+def bubble_sort_by(array)
+  sorted = false
+
+  until sorted
+    sorted = true
+
+    (0..array.length - 2).each do |i|
+      if yield(array[i], array[i + 1]).positive?
+        array[i], array[i + 1] = array[i + 1], array[i]
+        sorted = false
+      end
+    end
+  end
+  array
+end
+
+# array = Array.new(10) { rand(1...9) }
+array = bubble_sort_by(['hi', 'hello', 'hey']) do |left, right|
+  left.length - right.length
+end
